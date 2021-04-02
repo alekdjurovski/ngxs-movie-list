4d0f80e8582ec3c33a50fd7257186cb2f82b2d12
alekdjurovski <alek.djurovski@gmail.com>
2021-04-02 08:52:14 +0200

    start and setup project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReadComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,4 @@
+export interface MovieModel {
+    name: string;
+    url: string
+}
